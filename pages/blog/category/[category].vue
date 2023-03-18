245639a1651a055198e1db6695ae8f95f5e90128
Dan Vega <danvega@gmail.com>
2023-03-18 14:13:25 -0400

    updating category view for md and lg

--- pages/blog/category/[category].vue
+++ pages/blog/category/[category].vue
@@ -13,13 +13,11 @@ const { data: posts } = await useFetch<Post[]>("/api/notion/posts/" + encodeURI(
     <BlogCategoryHeader :category="category" />
 
     <section class=" bg-white">
-      <div class="mx-4 md:mx-auto max-w-6xl py-8">
+      <div class="mx-4 lg:mx-auto max-w-6xl py-8">
         <h3 class="text-3xl font-bold mb-8">Latest Posts in {{ category.name }}</h3>
-        <article class="grid grid-cols-1 md:grid-cols-3 md:gap-10">
+        <article class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-10">
           <div v-for="post in posts" :key="post.id" class="rounded-md shadow-md bg-slate-50">
             <NuxtLink :to="`/blog/${post.slug}`">
-              <!-- <img :src="`/images/blog/thumbnails/${post.cover}`" class="w-full h-48 object-cover rounded-md" /> -->
-              <!-- <img :src="post.cover" class="w-full h-48 object-cover rounded-md" /> -->
               <NuxtImg :src="post.cover" class="w-full h-48 object-cover rounded-md" sizes="sm:100vw md:50vw lg:400px" />
             </NuxtLink>
             <div class="p-6">
