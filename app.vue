638f08677fda3800e2c29a869b9d2836850259fd
Dan Vega <danvega@gmail.com>
2023-03-18 13:51:07 -0400

    changing mobile menu colors

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@
       <div>
         <button id="hamburger-button" class="relative h-8 w-8 cursor-pointer text-3xl md:hidden">
           <div
-            class="absolute top-4 -mt-0.5 h-1 w-8 rounded bg-black transition-all duration-500 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-3 before:rounded before:bg-black before:transition-all before:duration-500 before:content-[''] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-3 after:rounded after:bg-black after:transition-all after:duration-500 after:content-['']">
+            class="absolute top-4 -mt-0.5 h-1 w-8 rounded bg-primary transition-all duration-500 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-3 before:rounded before:bg-primary before:transition-all before:duration-500 before:content-[''] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-3 after:rounded after:bg-primary after:transition-all after:duration-500 after:content-['']">
           </div>
         </button>
         <nav class="hidden space-x-8 text-xl md:block " aria-label="main">
@@ -20,8 +20,8 @@
       </div>
     </section>
     <section id="mobile-menu"
-      class="top-68 justify-content-center absolute hidden w-full origin-top animate-open-menu flex-col bg-black text-3xl">
-      <nav class="flex min-h-screen flex-col items-center py-8 text-white" aria-label="mobile">
+      class="top-68 justify-content-center absolute hidden w-full origin-top animate-open-menu flex-col bg-primary text-3xl">
+      <nav class="flex min-h-screen flex-col items-center py-8 text-slate-900" aria-label="mobile">
         <NuxtLink to="/" class="w-full py-6 text-center hover:opacity-90">Home</NuxtLink>
         <NuxtLink to="/blog/category/momlife" class="w-full py-6 text-center hover:opacity-90">#MomLife</NuxtLink>
         <NuxtLink to="/blog/category/health-wellness" class="w-full py-6 text-center hover:opacity-90">Health & Wellness
