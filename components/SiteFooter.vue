c308133bdf4ac36f999bfb01b7887160db480cd6
Dan Vega <danvega@gmail.com>
2023-03-26 15:52:08 -0400

    adding convertkit newsletter forms

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import FooterNewsletterSignup from './blog/FooterNewsletterSignup.vue';
+
 
 </script>
 
@@ -10,16 +12,20 @@
           <div class="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 lg:max-w-none lg:grid-cols-2">
             <div class="max-w-xl lg:max-w-lg">
               <h2 class="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">Subscribe to my newsletter.</h2>
-              <p class="mt-4 text-lg leading-8 text-slate-900">Sign up for my monthly newsletter and stay up to date with
-                current blog posts.</p>
-              <div class="mt-6 flex max-w-md gap-x-4">
+            <p class="mt-4 text-lg leading-8 text-slate-900">Sign up for my monthly newsletter and stay up to date with
+              current blog posts.</p>
+
+            <FooterNewsletterSignup />
+
+            <!-- <div class="mt-6 flex max-w-md gap-x-4">
                 <label for="email-address" class="sr-only">Email address</label>
-                <input id="email-address" name="email" type="email" autocomplete="email" required
-                  class="min-w-0 flex-auto rounded-md border-0 bg-white px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                  placeholder="Enter your email">
-                <button type="submit"
-                  class="flex-none rounded-md bg-rose-200 py-2.5 px-3.5 text-sm font-semibold text-slate-900 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Subscribe</button>
-              </div>
+                  <input id="email-address" name="email" type="email" autocomplete="email" required
+                    class="min-w-0 flex-auto rounded-md border-0 bg-white px-3.5 py-2 text-slate-900 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    placeholder="Enter your email">
+                  <button type="submit"
+                    class="flex-none rounded-md bg-rose-200 py-2.5 px-3.5 text-sm font-semibold text-slate-900 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Subscribe</button>
+                </div> -->
+
             </div>
             <dl class="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
               <div class="flex flex-col items-start">
