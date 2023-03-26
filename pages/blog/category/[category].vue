6e6452bb737a0c21c0ceb565371ed3c384c0c3cf
Dan Vega <danvega@gmail.com>
2023-03-26 14:07:05 -0400

    cleaining up some minor errors

--- pages/blog/category/[category].vue
+++ pages/blog/category/[category].vue
@@ -2,7 +2,11 @@
 import categories from '@/data/categories.json';
 
 const categorySlug = useRoute().params.category;
-const category: Category = categories.find((c) => c.slug === categorySlug);
+const category: Category = categories.find((c) => c.slug === categorySlug)!;
+
+if (category == null) {
+  throw new Error(`Category ${categorySlug} not found`);
+}
 
 const { data: posts } = await useFetch<Post[]>("/api/notion/posts/" + encodeURI(category.notionCategory));
 
@@ -23,7 +27,7 @@ useServerSeoMeta({
 
     <section class=" bg-white">
       <div class="mx-4 lg:mx-auto max-w-6xl py-8">
-        <h3 class="text-3xl font-bold mb-8 font-cormorant font-bold">Latest Posts in {{ category.name }}</h3>
+        <h3 class="text-3xl mb-8 font-cormorant font-bold">Latest Posts in {{ category.name }}</h3>
         <article class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-10">
           <div v-for="post in posts" :key="post.id" class="rounded-md shadow-md bg-slate-50">
             <BlogPostCard :post="post" />
