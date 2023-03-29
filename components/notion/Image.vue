e25c09daabf240185c604c19b8210018918c2370
Dan Vega <danvega@gmail.com>
2023-03-28 22:37:11 -0400

    fixing img src attribute

--- components/notion/Image.vue
+++ components/notion/Image.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <NuxtImg :src="imgSrc" class="w-full my-4 zoom" sizes="sm:100vw md:50vw lg:1152px" /> -->
-  <img src="imgSrc" class="w-full my-4 zoom" alt="" />
+  <img :src="imgSrc" class="w-full my-4 zoom" alt="" />
 </template>
 
 <script setup lang="ts">
