a07290731d20f3cf375563b46c310ed7f00a2c4c
Dan Vega <danvega@gmail.com>
2023-05-29 09:18:59 -0400

    Adding links to bulleted list items

--- components/notion/BulletedList.vue
+++ components/notion/BulletedList.vue
@@ -1,11 +1,23 @@
 <template>
-  <ul :id="block.id">
-    <li>{{ block?.bulleted_list_item?.rich_text[0].plain_text }}</li>
-  </ul>
+  <li id:="block.id" v-html="listItem" class="pl-4"></li>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
   block: { type: Object as PropType<Block>, default: () => ({}) }
 });
+
+const listItem = ref('');
+const text = props.block?.bulleted_list_item?.rich_text[0];
+
+if (text?.text?.link != null) {
+  listItem.value += `<a href="${text?.text?.link?.url}" class="text-blue-500 hover:text-blue-700">${text.text.content}</a>`;
+} else {
+  if (text?.annotations?.bold) {
+    listItem.value += `<strong>${text.plain_text}</strong>`;
+  } else {
+    listItem.value += text?.plain_text;
+  }
+}
+
 </script>
