c75f7a178909f991872c0a372b2f161d385e33de
Dan Vega <danvega@gmail.com>
2023-10-10 09:31:34 -0400

    updating bulleted list to use types

--- components/notion/BulletedList.vue
+++ components/notion/BulletedList.vue
@@ -1,27 +1,30 @@
-<script setup lang="ts">
-import {BulletedListItemBlockObjectResponse} from "@notionhq/client/build/src/api-endpoints";
+<script setup lang="ts" xmlns:id="http://www.w3.org/1999/xhtml">
+import {
+  BulletedListItemBlockObjectResponse,
+  TextRichTextItemResponse
+} from "@notionhq/client/build/src/api-endpoints";
 
 const props = defineProps({
   block: { type: Object as PropType<BulletedListItemBlockObjectResponse> }
 });
 
-// const listItem = ref('');
-// const text = props.block?.bulleted_list_item?.rich_text[0];
-//
-// if (text?.text?.link != null) {
-//   listItem.value += `<a href="${text?.text?.link?.url}" class="text-blue-500 hover:text-blue-700">${text.text.content}</a>`;
-// } else {
-//   if (text?.annotations?.bold) {
-//     listItem.value += `<strong>${text.plain_text}</strong>`;
-//   } else {
-//     listItem.value += text?.plain_text;
-//   }
-// }
+// There currently is no way to wrap each bullet list group with <ul>
+const listItem = ref('');
+props.block.bulleted_list_item.rich_text.forEach((item : TextRichTextItemResponse) => {
 
+  if(item.text.link != null) {
+    listItem.value += `<a href="${item.text.link.url}" class="text-blue-500 hover:text-blue-700">${item.text.content}</a>`;
+  } else {
+    if(item.annotations.bold) {
+      listItem.value += `<strong>${item.plain_text}</strong>`;
+    } else {
+      listItem.value += item.plain_text;
+    }
+  }
+})
 </script>
 
-
 <template>
-<!--  <li id:="block.id" v-html="listItem" class="pl-4"></li>-->
+  <li :id="props.block.id" v-html="listItem" class="pl-4"></li>
 </template>
 
