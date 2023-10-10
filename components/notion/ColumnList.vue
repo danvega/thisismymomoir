ff31e4f99730e05789e79f1d2a7564611a621132
Dan Vega <danvega@gmail.com>
2023-10-10 09:04:26 -0400

    updating column list to use correct types

--- components/notion/ColumnList.vue
+++ components/notion/ColumnList.vue
@@ -1,35 +1,36 @@
 <script setup lang="ts">
-// import Image from './Image.vue';
-import {ColumnListBlockObjectResponse} from "@notionhq/client/build/src/api-endpoints";
+import Image from './Image.vue';
+import {
+  BlockObjectResponse,
+  ColumnBlockObjectResponse,
+  ColumnListBlockObjectResponse
+} from "@notionhq/client/build/src/api-endpoints";
 
 const props = defineProps({
   block: { type: Object as PropType<ColumnListBlockObjectResponse> }
 });
-//
-// const { data: blockList } = await useFetch<Block[]>(`/api/notion/blocks/${props.block.id}`);
-//
-// const blocks: Ref<Block[]> = ref([]);
-//
-// blockList.value?.forEach(async (block) => {
-//   const content = await getBlockDetails(block.id);;
-//   if (content.length == 1) {
-//     blocks.value.push(content[0]);
-//   }
-// });
-//
-// async function getBlockDetails(id: string): Promise<Block[]> {
-//   // const { data } = useFetch<Block[]>(`/api/notion/blocks/${id}`);
-//   const data: Block[] = await $fetch(`/api/notion/blocks/${id}`);
-//   return data || [];
-// }
+
+// This currently only works to display 2 column content of images, update as needed.
+const { data: columns } = await useFetch<ColumnBlockObjectResponse[]>(`/api/notion/blocks/${props.block.id}`);
+const blocks: Ref<BlockObjectResponse[]> = ref([]);
+columns.value.forEach( async (column) => {
+  const content = await getColumnDetails(column.id);
+  if(content.length == 1) {
+    blocks.value.push(content[0]);
+  }
+});
+
+async function getColumnDetails(id: string): Promise<BlockObjectResponse[]> {
+  return await $fetch(`/api/notion/blocks/${id}`);
+}
 </script>
 
 <template>
-<!--  <div class="grid grid-cols-2 gap-4">-->
-<!--    <div v-for="block in blocks" :key="block.id">-->
-<!--      <Image v-if="block.type === 'image'" :block="block" />-->
-<!--    </div>-->
-<!--  </div>-->
+  <section class="grid grid-cols-2 gap-4">
+    <div v-for="block in blocks" :key="block.id">
+      <Image v-if="block.type === 'image'" :block="block" />
+    </div>
+  </section>
 </template>
 
 
