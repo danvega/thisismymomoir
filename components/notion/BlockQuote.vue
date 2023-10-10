2a9f54ef69d51cb6ad759c04e18e603c6ac96ca7
Dan Vega <danvega@gmail.com>
2023-10-10 14:28:07 -0400

    Adding block quote

---
+++ components/notion/BlockQuote.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { QuoteBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+
+const props = defineProps({
+  block: { type: Object as PropType<QuoteBlockObjectResponse>}
+});
+</script>
+
+<template>
+  <blockquote class="text-xl italic font-semibold text-gray-900 p-8 bg-gray-100 rounded-2xl">
+    {{ props.block["quote"]["rich_text"][0].text.content}}
+  </blockquote>
+</template>
